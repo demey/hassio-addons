ffcef96830ac77126447334c329c1cbedbde624a
demey <35800187+demey@users.noreply.github.com>
2020-09-04 17:17:54 +0300

    Update run.sh

--- apcupsd2mqtt/rootfs/usr/bin/run.sh
+++ apcupsd2mqtt/rootfs/usr/bin/run.sh
@@ -34,7 +34,6 @@ main() {
         | reduce range(0; length/2) as $i 
           ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
       
-      echo "$message"
       mqtthost=$(echo "$(bashio::config 'mqtt')" | jq -r '."host"')
       mqttport=$(echo "$(bashio::config 'mqtt')" | jq -r '."port"')
       username=$(echo "$(bashio::config 'mqtt')" | jq -r '."username"')
