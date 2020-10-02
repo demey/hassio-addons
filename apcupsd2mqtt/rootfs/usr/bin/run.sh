3f2bcd3c99ca6d39adca2edd701b5e6a97239074
demey <igor.kochubinskiy@gmail.com>
2020-10-03 00:31:01 +0300

    Local UPS support function

--- apcupsd2mqtt/rootfs/usr/bin/run.sh
+++ apcupsd2mqtt/rootfs/usr/bin/run.sh
@@ -7,6 +7,11 @@ main() {
 
   local sleep
   sleep=$(bashio::config 'update_interval')
+  mqtthost=$(echo "$(bashio::config 'mqtt')" | jq -r '."host"')
+  mqttport=$(echo "$(bashio::config 'mqtt')" | jq -r '."port"')
+  username=$(echo "$(bashio::config 'mqtt')" | jq -r '."username"')
+  password=$(echo "$(bashio::config 'mqtt')" | jq -r '."password"')
+  topic=$(echo "$(bashio::config 'mqtt')" | jq -r '."topic"')
 
   while true; do
 
@@ -34,11 +39,6 @@ main() {
         | reduce range(0; length/2) as $i 
           ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
       
-      mqtthost=$(echo "$(bashio::config 'mqtt')" | jq -r '."host"')
-      mqttport=$(echo "$(bashio::config 'mqtt')" | jq -r '."port"')
-      username=$(echo "$(bashio::config 'mqtt')" | jq -r '."username"')
-      password=$(echo "$(bashio::config 'mqtt')" | jq -r '."password"')
-      topic=$(echo "$(bashio::config 'mqtt')" | jq -r '."topic"')
       fulltopic="${topic}${apcname}/status"
 
       mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t "$fulltopic" -m "$message"
