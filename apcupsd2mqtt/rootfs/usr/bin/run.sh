3a01452ae75854d595a7ff3f76eb8d515051e1d7
demey <igor.kochubinskiy@gmail.com>
2020-10-04 03:21:15 +0300

    Error handling

--- apcupsd2mqtt/rootfs/usr/bin/run.sh
+++ apcupsd2mqtt/rootfs/usr/bin/run.sh
@@ -21,28 +21,30 @@ main() {
     do
       apchost=$(echo "$k" | jq -r '."url"')
       apcname=$(echo "$k" | jq -r '."name"')
-      
-      readarray -t array <<< $(apcaccess -h "$apchost") || bashio::log.error "Connection to $apchost filed"
-
-      declare -A upsmap
-      for i in "${array[@]}"
-      do
-        IFS=': ' read -ra line <<< "$i"
-        key="$(echo -e "${line[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-        value="$(echo -e "${line[1]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-        upsmap["$key"]=$value
-      done
-      message=$(for key in "${!upsmap[@]}"; do
-        printf '%s\0%s\0' "$key" "${upsmap[$key]}"
-      done |
-      jq -Rs '
-        split("\u0000")
-        | . as $a
-        | reduce range(0; length/2) as $i 
-          ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
-      
       fulltopic="${topic}${apcname}/status"
 
+      readarray -t array <<< $(apcaccess -h "$apchost")
+      if [[ "${#array[@]}" -gt 0 ]]; then
+        declare -A upsmap
+        for i in "${array[@]}"
+        do
+          IFS=': ' read -ra line <<< "$i"
+          key="$(echo -e "${line[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+          value="$(echo -e "${line[1]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+          upsmap["$key"]=$value
+        done
+        message=$(for key in "${!upsmap[@]}"; do
+          printf '%s\0%s\0' "$key" "${upsmap[$key]}"
+        done |
+        jq -Rs '
+          split("\u0000")
+          | . as $a
+          | reduce range(0; length/2) as $i 
+            ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
+
+      else  
+        message=$(echo "{\"STATUS\":\"OFFLINE\"}")
+      fi
       mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t "$fulltopic" -m "$message"
     done
       
