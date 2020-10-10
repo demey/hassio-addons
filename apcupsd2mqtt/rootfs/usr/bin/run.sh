8e50cca74fadff553e6e0e5e7a03ebe721bf642b
demey <igor.kochubinskiy@gmail.com>
2020-10-10 20:22:07 +0300

    apcupsd availability

--- apcupsd2mqtt/rootfs/usr/bin/run.sh
+++ apcupsd2mqtt/rootfs/usr/bin/run.sh
@@ -16,10 +16,10 @@ main() {
   bashio::log.info "Service apcupsd2mqtt started"
 
   mqttstate="$(nmap -n -p $mqttport $mqtthost | grep $mqttport | awk '{print $2}')"
-  bashio::log.info "MQTT server port state: $mqttstate"
   
   while true; do
     if [[ "$mqttstate" == "open" ]]; then
+      bashio::log.info "MQTT server port state: $mqttstate"
 
       for k in $(echo "$(bashio::config 'network_upses')")
       do
@@ -27,28 +27,37 @@ main() {
         apcname=$(echo "$k" | jq -r '."name"')
         fulltopic="${topic}${apcname}/status"
 
-        readarray -t array <<< $(apcaccess -h "$apchost")
+        echo "${apchost#*:}"
+        echo "${apchost%:*}"
 
-        if [[ "${array[0]}" =~ "refused" ]]; then
-          message=$(echo "{\"STATUS\":\"OFFLINE\"}")
-          bashio::log.info "APC host $apchost unavailable"
-        else  
-          declare -A upsmap
-          for i in "${array[@]}"
-          do
-            IFS=': ' read -ra line <<< "$i"
-            key="$(echo -e "${line[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-            value="$(echo -e "${line[1]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
-            upsmap["$key"]=$value
-          done
-          message=$(for key in "${!upsmap[@]}"; do
-            printf '%s\0%s\0' "$key" "${upsmap[$key]}"
-          done |
-          jq -Rs '
-            split("\u0000")
-            | . as $a
-            | reduce range(0; length/2) as $i 
-              ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
+        upsstate="$(nmap -n -p ${apchost#*:} ${apchost%:*} | grep ${apchost#*:} | awk '{print $2}')"
+
+        if [[ "$upsstate" == "open" ]]; then
+          readarray -t array <<< $(apcaccess -h "$apchost")
+
+          if [[ "${array[0]}" =~ "refused" ]]; then
+            message=$(echo "{\"STATUS\":\"OFFLINE\"}")
+            bashio::log.info "APC host $apchost unavailable"
+          else  
+            declare -A upsmap
+            for i in "${array[@]}"
+            do
+              IFS=': ' read -ra line <<< "$i"
+              key="$(echo -e "${line[0]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+              value="$(echo -e "${line[1]}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+              upsmap["$key"]=$value
+            done
+            message=$(for key in "${!upsmap[@]}"; do
+              printf '%s\0%s\0' "$key" "${upsmap[$key]}"
+            done |
+            jq -Rs '
+              split("\u0000")
+              | . as $a
+              | reduce range(0; length/2) as $i 
+                ({}; . + {($a[2*$i]): ($a[2*$i + 1]|fromjson? // .)})')
+          fi
+        else
+          bashio::log.info "Apcupsd service is not available on host: $apchost"
         fi
         mosquitto_pub -h "$mqtthost" -p "$mqttport" -u "$username" -P "$password" -t "$fulltopic" -m "$message"
       done
