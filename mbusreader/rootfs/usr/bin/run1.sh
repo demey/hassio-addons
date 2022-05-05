c470acfdc5a41ab7d646b96e03b6b39ec8472d3f
Igor Kochubynskyi <35800187+demey@users.noreply.github.com>
2022-05-05 20:59:40 +0300

    mbusreader init

---
+++ mbusreader/rootfs/usr/bin/run1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Home Assistant Add-on: mbusreader
+# ==============================================================================
+
+bashio::log.info "Service mbusreader started"
+
+main() {
+
+  while true; do
+    sleep 60
+  done
+}
+main "$@"
