9d8dfb37ebbf47f23aaa6b3814efc2e563a8a43c
Igor Kochubynskyi <35800187+demey@users.noreply.github.com>
2022-07-12 17:28:57 +0300

    Update run1.sh

--- mbusreader/rootfs/usr/bin/run1.sh
+++ mbusreader/rootfs/usr/bin/run1.sh
@@ -7,6 +7,11 @@ bashio::log.info "Service mbusreader started"
 
 mkdir -p /share/mbusreader
 sleep=$(bashio::config 'update_interval')
+mqtthost=$(echo "$(bashio::config 'mqtt')" | jq -r '."host"')
+mqttport=$(echo "$(bashio::config 'mqtt')" | jq -r '."port"')
+username=$(echo "$(bashio::config 'mqtt')" | jq -r '."username"')
+password=$(echo "$(bashio::config 'mqtt')" | jq -r '."password"')
+topic=$(echo "$(bashio::config 'mqtt')" | jq -r '."topic"')
 
 main() {
 
