2157fb2f48ceddc8e9972f6dc0ac6440c40413b1
Igor Kochubynskyi <35800187+demey@users.noreply.github.com>
2022-07-20 13:33:36 +0300

    Update upload.py

--- ha2gd/upload.py
+++ ha2gd/upload.py
@@ -1,61 +1,103 @@
 import os
+import shutil
 from pydrive.drive import GoogleDrive 
 from pydrive.auth import GoogleAuth
 from datetime import datetime
+from signal import signal, SIGPIPE, SIG_DFL 
 
-workdir = "C:/Temp/ha2gd/"
+signal(SIGPIPE,SIG_DFL) 
+g_history_days = 9
+l_history_days = 3
+workdir = "/share/ha2gd/"
+os_root = "/config/media/camera/"
+gdrive_root_id = "" #fill it by own value
 
-def upload(path, folder_id):
-
-	""" Initial Authentication, local """
-	gauth = GoogleAuth()
-	gauth.LocalWebserverAuth()
-
-	#Upload handler for folder path and Grdrive folder
-	upload_handler(gauth, path, folder_id)
-
-	#with open('/share/ha2gd/update_log.txt', 'a') as update_log:
-	#	update_log.write( '\n' + 'Synced with GDrive on ' + str(datetime.now()))
 
 def upload_handler(gauth, os_root_path, gdrive_root_folder_id):
-	# Google authentication 
-	drive = GoogleDrive(gauth)
-	dir_dict = {os_root_path: gdrive_root_folder_id}
-	item_list = drive.ListFile({'q': "'{gdrive_root_folder_id}' in parents and trashed=false".format(gdrive_root_folder_id=gdrive_root_folder_id)}).GetList()
-	titles = []
-	for item in item_list:
-		titles.append(item['title'])
-	#print(type(item_list))
 
-	#For each dir path starting from os_root_path by os.walk()
-	for dirpath, dirnames, files in os.walk(os_root_path):
-		#print(dirpath, dirnames)
-		#print(type(files))
-		for file in files:
-			#print(dir_dict, file, str('in ' + dirpath))
-			if file not in titles:
-				new_file = drive.CreateFile({'title': file, "parents": [{"kind": "drive#fileLink", "id": dir_dict[dirpath]}]})  # Create GoogleDriveFile instance to parentID.
-				file_path = os.path.join(dirpath, file)
-				new_file.SetContentFile(file_path) # Set content of the file from given string.
-				new_file.Upload()
-				with open(workdir + 'update_log.txt', 'a') as update_log:
-					update_log.write( '\n' + 'Uploaded file to GD: %s %s' % (file, str(datetime.now())))
-				#print('Uploaded file to GD: %s' % (file))
-	for item in item_list:
-		if item['title'] not in files:
-			delete_item = drive.CreateFile({'id': item['id']})
-			delete_item.Delete()
-			with open((workdir + 'update_log.txt', 'a') as update_log:
-				update_log.write( '\n' + 'Deleted file on GD: %s %s' % (item['title'], str(datetime.now())))
-			#print('Deleted file on GD: %s' % (item['title']))
+  drive = GoogleDrive(gauth)
+  gdrive_item_list = drive.ListFile({'q': "'{0}' in parents and trashed=false".format(gdrive_root_folder_id)}).GetList()
+  g_folders = {}
+  for item in gdrive_item_list:
+    if item['mimeType'].endswith('vnd.google-apps.folder'):
+      g_folders[item['title']] = item['id']
 
-""" If you decide to hard code the Google Drive folder ID and Directory Path into the upload function 
-	you dont have to read the folder_sync_registrer below. You can just remove the code and uncomment
-	the upload call below this section
-"""
+  if len(g_folders) > g_history_days:
+    oldest_folder = min(g_folders.keys())
+    delete_item = drive.CreateFile({'id': g_folders[oldest_folder]})
+    delete_item.Delete()
+    with open(workdir + 'update_log.txt', 'a') as update_log:
+      update_log.write( '\n' + 'Deleted gdrive folder: %s %s' % (oldest_folder, str(datetime.now())))
+    g_folders.pop(oldest_folder, None)
 
-import csv
-with open(workdir + 'folder_sync_registrer.txt', 'r') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-    	upload(row[0], row[1])
+  l_folders = os.listdir(os_root_path)
+
+  if len(l_folders) > l_history_days:
+    shutil.rmtree(os_root_path + min(l_folders))
+    with open(workdir + 'update_log.txt', 'a') as update_log:
+      update_log.write( '\n' + 'Deleted local folder: %s %s' % (min(l_folders), str(datetime.now())))
+    l_folders.remove(min(l_folders))
+
+  for l_folder in l_folders:
+    if l_folder not in g_folders.keys():
+      new_g_folder_id = create_folder(drive, l_folder, gdrive_root_folder_id)
+      with open(workdir + 'update_log.txt', 'a') as update_log:
+        update_log.write( '\n' + 'Created new gdrive folder: %s %s' % (l_folder, str(datetime.now())))
+      g_folders[l_folder] = new_g_folder_id
+
+  newest_folder = max(g_folders.keys())
+#  print(newest_folder)
+  g_folder_item_list = drive.ListFile({'q': "'{0}' in parents and trashed=false".format(g_folders[newest_folder])}).GetList()
+  g_folder_item_names = []
+  for item in g_folder_item_list:
+    g_folder_item_names.append(item['title'])
+
+  os_item_list = os.listdir(os_root_path + newest_folder)
+#  print(os_item_list)
+  for item in os_item_list:
+    if item not in g_folder_item_names:
+      new_file = drive.CreateFile({'title': item, "parents": [{"kind": "drive#fileLink", "id": g_folders[newest_folder]}]})
+      file_path = os.path.join(os_root_path + newest_folder, item)
+      new_file.SetContentFile(file_path)
+      new_file.Upload()
+      with open(workdir + 'update_log.txt', 'a') as update_log:
+        update_log.write( '\n' + 'Uploaded file to GD: %s %s' % (item, str(datetime.now())))
+
+
+def create_folder(drive, folder_name, parent_folder_id):
+    
+  folder_metadata = {
+      'title': folder_name,
+      'mimeType': 'application/vnd.google-apps.folder',
+      'parents': [{"kind": "drive#fileLink", "id": parent_folder_id}]
+    }
+
+  folder = drive.CreateFile(folder_metadata)
+  folder.Upload()
+  return folder['id']
+
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile(workdir + 'credentials.json')
+if gauth.credentials is None:
+  # Authenticate if they're not there
+  # This is what solved the issues:
+  gauth.GetFlow()
+  gauth.flow.params.update({'access_type': 'offline'})
+  gauth.flow.params.update({'approval_prompt': 'force'})
+
+  gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+  # Refresh them if expired
+  gauth.Refresh()
+else:
+  # Initialize the saved creds
+  gauth.Authorize()
+
+# Save the current credentials to a file
+gauth.SaveCredentialsFile(workdir + 'credentials.json')
+
+#gauth.LocalWebserverAuth()
+upload_handler(gauth, os_root, gdrive_root_id)
+
