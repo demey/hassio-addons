3dc4fea4bc3874549ebe7ed0be3778824a935bb5
demey <igor.kochubinskiy@gmail.com>
2022-08-10 22:49:49 +0300

    Refactoring

--- shellsensors/rootfs/usr/bin/run1.sh
+++ shellsensors/rootfs/usr/bin/run1.sh
@@ -13,46 +13,50 @@ if [ -f $LOG_FILE ]; then
 fi 
 
 SLEEP=$(bashio::config 'update_interval')
+UPTIME=$(date -d "$(stat /proc/1/cmdline | grep 'Change' | sed "s/.000000000//" | awk '{print $2" "$3}')" +"%s")
 
 main() {
   while true; do
 
     MIN=$(date +"%M")
+    NOW=$(date +"%s")
+    DIFF=$(($NOW-$UPTIME))
     UVI_TIME=("00" "20" "40")
 
-    if [[ "${UVI_TIME[*]}" =~ "${MIN}" ]]; then 
+    if [[ "${UVI_TIME[*]}" =~ "${MIN}" && $DIFF -lt 29 ]]; then 
 
       SENSOR_NAME="sensor.uvi_current"
       UVI_VALUE=$(/share/shellsensors/uvi.pl)
       SENSOR_DATA='{"state": "'"$UVI_VALUE"'", "attributes": {"friendly_name":"'"УФ Індекс"'","icon":"mdi:sun-wireless","state_class":"measurement"}}'
 
-      curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
-         -s \
-         -o /dev/null \
-         -H "Content-Type: application/json" \
-         -d "$SENSOR_DATA" \
-         -w "[$(date)][INFO] Sensor update response code: %{http_code}\n" \
-         http://supervisor/core/api/states/${SENSOR_NAME}
+      send_sensor_data
+    
     fi
 
     FORECAST_TIME=("00" "10" "20" "30" "40" "50")
 
-    if [[ "${FORECAST_TIME[*]}" =~ "${MIN}" ]]; then 
+    if [[ "${FORECAST_TIME[*]}" =~ "${MIN}" && $DIFF -lt 29 ]]; then 
 
-      SENSOR_NAME="sensoropenweather_current"
+      SENSOR_NAME="sensor.openweather_current"
       UFORECAST_VALUE=$(/share/shellsensors/weather.pl)
       SENSOR_DATA='{"state": "'"$UFORECAST_VALUE"'", "attributes": {"friendly_name":"'"Прогноз погоди"'","icon":"mdi:weather-partly-cloudy"}}'
 
-      curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
-         -s \
-         -o /dev/null \
-         -H "Content-Type: application/json" \
-         -d "$SENSOR_DATA" \
-         -w "[$(date)][INFO] Sensor update response code: %{http_code}\n" \
-         http://supervisor/core/api/states/${SENSOR_NAME}
+      send_sensor_data
+
     fi
 
     sleep "${SLEEP}"
   done
 }
+
+send_sensor_data() {
+  curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
+     -s \
+     -o /dev/null \
+     -H "Content-Type: application/json" \
+     -d "$SENSOR_DATA" \
+     -w "[$(date)][INFO] Sensor update response code: %{http_code}\n" \
+     http://supervisor/core/api/states/${SENSOR_NAME}
+}
+
 main "$@"
