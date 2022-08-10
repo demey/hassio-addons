fe7fce01d473e0729efc3241371b66fd5695bfef
demey <igor.kochubinskiy@gmail.com>
2022-08-10 20:21:14 +0300

    version with 1 sensor

--- shellsensors/rootfs/usr/bin/run1.sh
+++ shellsensors/rootfs/usr/bin/run1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Home Assistant Add-on: mbusreader
+# Home Assistant Add-on: shellsensors
 # ==============================================================================
 
 bashio::log.info "Service shellsensors started"
@@ -16,6 +16,18 @@ SLEEP=$(bashio::config 'update_interval')
 
 main() {
   while true; do
+    SENSOR_NAME="sensor.uvi_current"
+    UVI_VALUE=$(/share/shellsensors/uvi.pl)
+    SENSOR_DATA='{"state": "'"$UVI_VALUE"'", "attributes": {"friendly_name":"'"УФ Індекс"'","icon":"mdi:sun-wireless","state_class":"measurement"}}'
+
+    curl -X POST -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
+       -s \
+       -o /dev/null \
+       -H "Content-Type: application/json" \
+       -d "$SENSOR_DATA" \
+       -w "[$(date)][INFO] Sensor update response code: %{http_code}\n" \
+       http://supervisor/core/api/states/${SENSOR_NAME}
+
     sleep "${SLEEP}"
   done
 }
