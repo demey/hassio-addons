8c5544e2a7580f3243682206723b0d5c0cd289be
demey <igor.kochubinskiy@gmail.com>
2022-08-11 17:48:01 +0300

    dev testing

--- apache2-perl-server/rootfs/usr/bin/run1.sh
+++ apache2-perl-server/rootfs/usr/bin/run1.sh
@@ -1,12 +1,18 @@
-#!/command/with-contenv bashio
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Home Assistant Add-on: Apache HTTP Server
 # ==============================================================================
 
+mkdir -p /share/apache2
+mkdir -p /share/apache2/logs
+mkdir -p /share/apache2/html
+mkdir -p /share/apache2/cgi-bin
+cp -r /var/www/modules /share/apache2
+/usr/sbin/httpd -k start
 
 main() {
   while true; do
-    sleep 60
+    sleep 600
   done
 }
 main "$@"
