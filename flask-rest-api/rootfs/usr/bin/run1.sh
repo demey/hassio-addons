7e77e516ca2f7ad95dc249bece799a583c5772ab
demey <igor.kochubinskiy@gmail.com>
2023-05-01 13:03:10 +0300

    flask-rest-api

--- flask-rest-api/rootfs/usr/bin/run1.sh
+++ flask-rest-api/rootfs/usr/bin/run1.sh
@@ -3,14 +3,14 @@
 # Home Assistant Add-on: Flask Application
 # ==============================================================================
 
-#export FLASK_APP=/share/main.py
-#flask run --host=0.0.0.0
+cd /share/flask
+flask --app main run --host=0.0.0.0 -p 5001
 
-main() {
-  while true; do
+#main() {
+#  while true; do
 #    echo ${SUPERVISOR_TOKEN} > /tmp/token.txt
 #    chmod 0644 /tmp/token.txt
-    sleep 100
-  done
-}
-main "$@"
+#    sleep 100
+#  done
+#}
+#main "$@"
