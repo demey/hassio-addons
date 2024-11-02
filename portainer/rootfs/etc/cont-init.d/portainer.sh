8a8264a684bfd11b24335d4c81c0d75b06d165a4
Igor Kochubynskyi <Igor.Kochubinskiy@gmail.com>
2024-11-02 23:49:15 +0200

    Convert to yaml and update base image

--- portainer/rootfs/etc/cont-init.d/portainer.sh
+++
@@ -1,6 +0,0 @@
-#!/command/with-contenv bashio
-# ==============================================================================
-# Home Assistant Private Add-on: Portainer
-# Runs some initializations for Portainer
-# ==============================================================================
-bashio::require.unprotected
