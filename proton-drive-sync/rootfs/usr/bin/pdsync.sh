21470b9ae717977c2582af507c486674abb8d86f
Igor Kochubynskyi <Igor.Kochubinskiy@gmail.com>
2025-05-20 20:14:10 +0300

    sensor added

--- proton-drive-sync/rootfs/usr/bin/pdsync.sh
+++ proton-drive-sync/rootfs/usr/bin/pdsync.sh
@@ -22,30 +22,34 @@ main() {
 
   while true; do
     used=`rclone about protondrive: | grep Used: | awk '{print $2}'`
-    bashio::log.info "Space used on drive: ${used} Gb"
+    curl -s \
+      -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
+      -H "Content-Type: application/json" \
+      -d '{"state": ${used}, "attributes": {"friendly_name": "Proton Drive used space", "unit_of_measurement": "Gb", "icon": "mdi:cloud-upload"}}' \
+      http://supervisor/api/states/sensor.pdrive_used_space \
+    /
+#    bashio::log.info "Space used on drive: ${used} Gb"
 
     for folder in $(bashio::config 'folders|keys'); do
-      bashio::log.info "Working with ${folder}"
-#      bashio::config.require.local_folder "folders[${folder}].local_folder"
-#      bashio::config.require.remote_folder "folders[${folder}].remote_folder"
-
       local_folder=$(bashio::config "folders[${folder}].local_folder")
       remote_folder=$(bashio::config "folders[${folder}].remote_folder")
 
       oldest=`ls -tcr $local_folder | head -1`
       bashio::log.info "Oldest folder is ${local_folder}/${oldest}"
 
-#      while [ `ls -1 | wc -l` -gt $days ]; do
-#        oldest=`ls -tcr $local_folder | head -1`
-#        echo "remove $oldest"
-#        rm -rf $oldest
-#      done
+     while [ `ls -1 | wc -l` -gt $days ]; do
+       oldest=`ls -tcr $local_folder | head -1`
+       bashio::log.info "Removing oldest folder ${local_folder}/${oldest}"
+       rm -rf "${local_folder}/${oldest}"
+     done
 
       bashio::log.info "Syncing ${local_folder} with ${remote_folder}"
       result=`rclone sync --protondrive-replace-existing-draft=true $local_folder protondrive:$remote_folder`
 
       if [ $? -ne 0 ]; then
         bashio::log.warn ${result}
+      else
+        bashio::log.info "Successful sync"
       fi
     done
     sleep "${sleep}"
